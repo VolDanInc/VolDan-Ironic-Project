d25c5dc925f8dd14e460f9192b3321addc251182
VKhodyrev <vkhodyrev1@gmail.com>
2022-10-07 21:13:20 +0300

    usermodel authroutes login & signup pages changed

--- models/User.model.js
+++ models/User.model.js
@@ -3,11 +3,19 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    username: {
+    email: {
       type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      //match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+      lowercase: true,
+      trim: true
       // unique: true -> Ideally, should be unique, but its up to you
     },
-    password: String,
+    passwordHash: {
+      type: String,
+      required: [true, 'Password is required.']
+    }
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -19,11 +19,11 @@ router.get("/signup", isLoggedOut, (req, res) => {
 });
 
 router.post("/signup", isLoggedOut, (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!username) {
+  if (!email) {
     return res.status(400).render("auth/signup", {
-      errorMessage: "Please provide your username.",
+      errorMessage: "Please provide your email.",
     });
   }
 
@@ -46,12 +46,12 @@ router.post("/signup", isLoggedOut, (req, res) => {
   */
 
   // Search the database for a user with the username submitted in the form
-  User.findOne({ username }).then((found) => {
+  User.findOne({ email }).then((found) => {
     // If the user is found, send the message username is taken
     if (found) {
       return res
         .status(400)
-        .render("auth/signup", { errorMessage: "Username already taken." });
+        .render("auth/signup", { errorMessage: "This Email already exists." });
     }
 
     // if user is not found, create a new user - start with hashing the password
@@ -60,9 +60,10 @@ router.post("/signup", isLoggedOut, (req, res) => {
       .then((salt) => bcrypt.hash(password, salt))
       .then((hashedPassword) => {
         // Create a user and save it in the database
+        console.log(hashedPassword);
         return User.create({
-          username,
-          password: hashedPassword,
+          email,
+          passwordHash: hashedPassword,
         });
       })
       .then((user) => {
@@ -93,9 +94,9 @@ router.get("/login", isLoggedOut, (req, res) => {
 });
 
 router.post("/login", isLoggedOut, (req, res, next) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
-  if (!username) {
+  if (!email) {
     return res
       .status(400)
       .render("auth/login", { errorMessage: "Please provide your username." });
@@ -110,7 +111,7 @@ router.post("/login", isLoggedOut, (req, res, next) => {
   }
 
   // Search the database for a user with the username submitted in the form
-  User.findOne({ username })
+  User.findOne({ email })
     .then((user) => {
       // If the user isn't found, send the message that user provided wrong credentials
       if (!user) {
@@ -120,7 +121,7 @@ router.post("/login", isLoggedOut, (req, res, next) => {
       }
 
       // If user is found based on the username, check if the in putted password matches the one saved in the database
-      bcrypt.compare(password, user.password).then((isSamePassword) => {
+      bcrypt.compare(password, user.passwordHash).then((isSamePassword) => {
         if (!isSamePassword) {
           return res
             .status(400)
