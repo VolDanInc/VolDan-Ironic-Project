c25f6367aae4d1ec08507d6cc51c7755757b3977
VKhodyrev <vkhodyrev1@gmail.com>
2022-10-10 22:47:03 +0300

    added spotify topics to home page

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -3,15 +3,26 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 
 const isLoggedIn = require("../middleware/isLoggedIn");
+const SpotifyWebApi = require('spotify-web-api-node');
+//const articles = {
+//    method: 'GET',
+//    url: 'https://myallies-breaking-news-v1.p.rapidapi.com/GetTopNews',
+//    headers: {
+//      'X-RapidAPI-Key': '97d47758bdmsh09580f5807389ddp1a1c72jsn7a9c4b3ce6ac',
+//      'X-RapidAPI-Host': 'community-hacker-news-v1.p.rapidapi.com'
+//    }
+//  };
 
-const articles = {
-    method: 'GET',
-    url: 'https://myallies-breaking-news-v1.p.rapidapi.com/GetTopNews',
-    headers: {
-      'X-RapidAPI-Key': '97d47758bdmsh09580f5807389ddp1a1c72jsn7a9c4b3ce6ac',
-      'X-RapidAPI-Host': 'community-hacker-news-v1.p.rapidapi.com'
-    }
-  };
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+    .clientCredentialsGrant()
+    .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+    .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 router.post("/books", (req, res) => {
 
@@ -19,22 +30,30 @@ router.post("/books", (req, res) => {
         case "movies":
 
             break;
-        case "articles":
-            axios
-                .request(articles)
-                .then((articlesFromExt) => {
-                    //const booksPreview = books.data.items;
-                    console.log(articlesFromExt);
-                    res.render("external/articles");
+        case "music":
+            spotifyApi.getNewReleases({ limit: 10, offset: 0})
+                .then((data) => {
+                    
+                    const items = data.body.albums.items;
+                    //console.log(items[2].artists);
+                    res.render("external/music", {arrOfItems: items});
                 })
                 .catch((err) => console.log(err));
+            //axios
+            //    .request(articles)
+            //    .then((articlesFromExt) => {
+            //        //const booksPreview = books.data.items;
+            //        console.log(articlesFromExt);
+            //        res.render("external/articles");
+            //    })
+            //    .catch((err) => console.log(err));
             break;
         case "books":
             axios
                 .get("https://www.googleapis.com/books/v1/volumes?q=search+terms")
                 .then((books) => {
                     const booksPreview = books.data.items;
-                    console.log(books.data.items[0].volumeInfo.imageLinks);
+                    console.log(books.data.items[0].volumeInfo);
                     res.render("external/books", { booksFromNet: booksPreview });
                 })
                 .catch((err) => console.log(err));
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,16 +1,39 @@
 const router = require("express").Router();
 const axios = require("axios");
+const SpotifyWebApi = require('spotify-web-api-node');
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET
+});
+
+// Retrieve an access token
+spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
 /* GET home page */
 router.get("/", (req, res, next) => {
 
   axios
-  .get("https://www.googleapis.com/books/v1/volumes?q=search+terms")
-  .then((books) => {
+    .get("https://www.googleapis.com/books/v1/volumes?q=search+terms")
+    .then((books) => {
       const booksPreview = books.data.items;
       //console.log(books.data.items[0].volumeInfo.imageLinks);
-      res.render("index", { booksFromNet: booksPreview });
-  })
-  .catch((err) => console.log(err));
+      //res.render("index", { booksFromNet: booksPreview });
+      return booksPreview;
+    })
+    .then((books) => {
+      spotifyApi.getNewReleases({ limit: 10, offset: 0 })
+        .then((data) => {
+          const items = data.body.albums.items;
+          //console.log(items[2].artists);
+          res.render("index", { arrOfItems: items,  booksFromNet: books});
+        })
+        .catch((err) => console.log(err));
+        
+    })
+    .catch((err) => console.log(err));
+
 
 
 });
