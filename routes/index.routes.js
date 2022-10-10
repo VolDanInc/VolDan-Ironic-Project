8da0935898b576d66eba59a0f9de49da12c20bf7
VKhodyrev <vkhodyrev1@gmail.com>
2022-10-10 13:47:10 +0300

    applying home page

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
-
+const axios = require("axios");
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+
+  axios
+  .get("https://www.googleapis.com/books/v1/volumes?q=search+terms")
+  .then((books) => {
+      const booksPreview = books.data.items;
+      //console.log(books.data.items[0].volumeInfo.imageLinks);
+      res.render("index", { booksFromNet: booksPreview });
+  })
+  .catch((err) => console.log(err));
+
+
 });
 
 module.exports = router;
